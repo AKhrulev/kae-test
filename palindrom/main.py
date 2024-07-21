24859b8e30e551eab173a23be42a6c7cb01864fa
Alexey Khrulev <alexey52@gmail.com>
2024-07-21 21:16:12 +0300

    Remove unuded lines of code

--- palindrom/main.py
+++ palindrom/main.py
@@ -1,11 +1,4 @@
 import random
-lst = [str(random.randint(0, 9)) for i in range(1000)]
-prefix1 = ''.join(lst)
-random.shuffle(lst)
-prefix2 = ''.join(lst)
-random.shuffle(lst)
-prefix3 = ''.join(lst)
-s = prefix1 + prefix2 + prefix2[::-1]
 
 #s = 'k2e2z8p2xjr0rjx2p_ul93lgbzprqqzpdwnwdpzqqrpzexu_kr32yhd_pit8dwnm_ec72ac_01u310t404mfvyb_vajehda_8f4qvszmm_jy6yfy6yj_mmzsvq4f8_adhed2tiev9rv9sbo8e1r_bo864'
 #s = prefix + s
